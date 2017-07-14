b3cd793b9676ec6fb618cf68dcd5ad37a0136400
jade1314 <wangyu1001@live.com>
2017-07-14 12:23:01 +0800

    Initial Commit

---
+++ SystemFontExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SystemFontExample
+//
+//  Created by 王玉 on 2017/7/14.
+//  Copyright © 2017年 王玉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SystemFontExample/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SystemFontExample
+//
+//  Created by 王玉 on 2017/7/14.
+//  Copyright © 2017年 王玉. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
